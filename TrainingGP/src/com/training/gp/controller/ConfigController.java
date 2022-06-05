3be41175fdf3387271817eb80095c9c5e3c043ee
Christo <ChristoOpperman0@gmail.com>
2022-06-05 12:25:38 +0200

    Training GP - Added generic operators

--- TrainingGP/src/com/training/gp/controller/ConfigController.java
+++ TrainingGP/src/com/training/gp/controller/ConfigController.java
@@ -2,8 +2,13 @@ package com.training.gp.controller;
 
 public class ConfigController {
     private static int maxDepth =5;
+    private static int populationSize = 100000;
 
     public static int getMaxDepth() {
         return maxDepth;
+    }
+
+    public static int getPopulationSize() {
+        return populationSize;
     }
 }
